6a2d15fce3c8f8d47d9b4bf95452700193363c06
Abhishek Tiwari <homedevendra@gmail.com>
2026-09-12 23:58:40 -0700

    navbar and KPI created

--- Client/src/Pages/home/Home.jsx
+++ Client/src/Pages/home/Home.jsx
@@ -1,5 +1,9 @@
 import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
+
+
 import './home.scss'
+import Widgets from '../../components/widgets/Widgets'
 
 function Home (){
 
@@ -8,7 +12,14 @@ function Home (){
     <div className="home">
         <Sidebar/>
         <div className="homecontainer">
-            container
+            <Navbar/>
+            <div className="widgets">
+                <Widgets type="user"/>
+                <Widgets type="order"/>
+                <Widgets type="earning"/>
+                <Widgets type="balance"/>
+              
+            </div>
         </div>
 
     </div>
--- Client/src/components/navbar/Navbar.jsx
+++ Client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,60 @@
-import  './navbar.scss'
-function Navbar(){
+import './navbar.scss'
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import ListIcon from '@mui/icons-material/List';
+import ad from '../../assets/Images/ad.jpg'
 
-    return(
+
+function Navbar() {
+
+    return (
 
         <div className="navbar">
-            Title
+            <div className="wrapper">
+                <div className="search">
+                    <input type="text" placeholder='search...' />
+                    <SearchIcon />
+
+
+                </div>
+                <div className="items">
+                    <div className="item">
+                        <LanguageIcon className="icon"/>
+                        English
+                    </div>
+                    <div className="item">
+                        <DarkModeIcon className="icon"/>
+                    
+                    </div>
+                    <div className="item">
+                        <FullscreenExitIcon className="icon"/>
+                        
+                    </div>
+                    <div className="item">
+                        <NotificationsIcon className="icon"/>
+                        <div className="counter">1</div>
+                       
+                    </div>
+                    <div className="item">
+                        <ChatBubbleIcon className="icon"/>
+                        <div className="counter">2</div>
+                    </div>
+                    <div className="item">
+                        <ListIcon/>
+                    </div>
+                    <div className="item">
+                        <img src={ad} alt="image not found" className='avtar'/>
+                       
+                    </div>
+                </div>
+
+
+
+            </div>
         </div>
     )
 }
